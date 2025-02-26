e71d8b54a8b74cb347cd09aa88b1dd30180a8f8d
Paolo BERTINI <bertin_p@etna-alternance.net>
2025-02-26 14:21:25 +0100

    mise en ordre

--- premier_projet/main.cs
+++ premier_projet/main.cs
@@ -10,9 +10,10 @@ class Program
         // x++ != ++x; priorité en fonction si c'est un sufixe ou un préfixe
         // WriteLine(++x)      =       WriteLine(x + 1);
         // WriteLine(x++)      =       WriteLine(x);  x += 1;
+        Console.WriteLine($"Hello World ! I'm {x} {moi}");
+        
         bool test = false; // pas False
         test = true; // pas True
         Console.WriteLine(!test); // ! = not
-        Console.WriteLine($"Hello World ! I'm {x} {moi}");
     }
 }
